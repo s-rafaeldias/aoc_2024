db9e3e6b0d8f34c8c7b07a1114fb711a34e9b107
Rafael Dias Silveira <s.rafaeldias@protonmail.com>
2024-12-01 23:27:58 +0100

    refactor(01): better pipe usage

--- lib/day01.ex
+++ lib/day01.ex
@@ -33,7 +33,7 @@ defmodule Day01 do
       [l, r] =
         line
         |> String.split()
-        |> Enum.map(&(&1 |> String.to_integer()))
+        |> Enum.map(&String.to_integer/1)
 
       {[l | left], [r | right]}
     end)
